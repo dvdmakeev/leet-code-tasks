2eef73e262e48ccc522884683aba7f0ecb4ad4c0
dmakeev <dmakeev@intermedia.net>
2021-02-01 22:21:46 +0300

    naive solution two some problem

---
+++ 1-two-sum/main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i, a in enumerate(nums):
+            for j, b in enumerate(nums[(i+1):]):
+                if a + b == target:
+                    return [i, j + i + 1]
+
+        return []
