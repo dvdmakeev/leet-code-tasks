b756039f62fbeaae63a8fea59385a541c0fba3c4
dvdmakeev <dvdmakeev@gmail.com>
2021-02-18 01:48:54 +0300

    1480-running-num-1d-array c sharp solution

---
+++ 1480-running-num-1d-array/Program.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+	{
+        for (int i = 0; i < nums.Length - 1; ++i)
+        {
+            nums[i + 1] += nums[i];
+        }
+        
+        return nums;
+    }
+}
