2d205413fe5256eb7fe5bf76e3d97cb30fc76890
dvdmakeev <dvdmakeev@gmail.com>
2021-02-27 17:21:52 +0300

    best time to buy and sell stock csharp solution

---
+++ 121-best-time-to-buy-and-sell-stock/Program.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = int.MaxValue;
+        
+        for (int i = 0; i < prices.Length; ++i)
+        {
+            if (prices[i] < minPrice)
+            {
+                minPrice = prices[i];
+            }
+            else if (maxProfit < prices[i] - minPrice)
+            {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
