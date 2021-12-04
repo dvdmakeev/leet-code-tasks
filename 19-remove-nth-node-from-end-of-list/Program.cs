c30af6a9b337741c5910121fa374cc2950d7390d
dvdmakeev <dvdmakeev@gmail.com>
2021-12-04 15:18:52 +0300

    19-remove-nth-node-from-end-of-list one pass solution

--- 19-remove-nth-node-from-end-of-list/Program.cs
+++ 19-remove-nth-node-from-end-of-list/Program.cs
@@ -9,6 +9,44 @@
  *     }
  * }
  */
+ public class SolutionOnePass
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n) 
+    {
+        ListNode preN = null;
+        var node = head;
+        int i = 0;
+        while (node != null)
+        {
+            if (i < n)
+            {
+                ++i;
+            }
+            else if (i == n)
+            {
+                preN = head;
+                ++i;
+            }
+            else
+            {
+                preN = preN.next;
+            }
+            
+            node = node.next;
+        }
+        if (preN != null)
+        {
+            preN.next = preN.next.next;
+        }
+        else
+        {
+            head = head.next;
+        }
+        
+        return head;
+    }
+}
+
 public class Solution 
 {
     public ListNode RemoveNthFromEnd(ListNode head, int n) 
