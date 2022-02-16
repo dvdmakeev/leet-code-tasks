d28a766d687af7ae1d2d4cc0e0817c13bc133102
dvdmakeev <dvdmakeev@gmail.com>
2022-02-16 23:16:17 +0300

    1920-build-array-from-permutation

---
+++ 1920-build-array-from-permutation/Program.cs
@@ -0,0 +1,12 @@
+public class Solution 
+{
+    public int[] BuildArray(int[] nums) 
+    {
+        var ans = new int[nums.Length];
+        for (int i = 0; i < ans.Length; ++i)
+        {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+}
